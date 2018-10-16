4851f946b1f255783bc7e79e1b731b33f4bf915d
paulanto09009 <paul@w5y.net>
2018-10-16 14:33:31 -0400

    Update algo.py

--- algo.py
+++ algo.py
@@ -184,7 +184,7 @@ def my_rebalance(context, data):
     
     # Order sell at profit target in hope that somebody actually buys it
     for stock in context.portfolio.positions:
-        log.info(stock,context.age[stock])          
+        print(stock)          
         if not get_open_orders(stock):
             StockShares = context.portfolio.positions[stock].amount
             CurrPrice = float(data.current([stock], 'price'))
@@ -221,7 +221,6 @@ def my_rebalance(context, data):
                           style=LimitOrder(SellPrice)
                           )
             else:
-                log.info(stock,context.age[stock])  
                 if (stock in context.age and context.age[stock] < 2):
                     pass
                 elif stock not in context.age:
