9aa7759293fd6f73f3df54e12002fc111e80bdf0
paulanto09009 <paul@w5y.net>
2018-10-16 13:30:02 -0400

    Update algo.py

--- algo.py
+++ algo.py
@@ -1,41 +1,28 @@
-from pylivetrader.api import (
-    attach_pipeline,
-    date_rules,
-    time_rules,
-    order,
-    get_open_orders,
-    cancel_order,
-    pipeline_output,
-    schedule_function,
-)
-from pipeline_live.data.iex.pricing import USEquityPricing
-from pipeline_live.data.iex.fundamentals import IEXCompany, IEXKeyStats
-from pipeline_live.data.iex.factors import (
-    SimpleMovingAverage, AverageDollarVolume,
-)
-from pipeline_live.data.polygon.filters import (
-    IsPrimaryShareEmulation as IsPrimaryShare,
-)
-from pylivetrader.finance.execution import LimitOrder
-from zipline.pipeline import Pipeline
+from quantopian.pipeline import Pipeline
+from quantopian.algorithm import attach_pipeline, pipeline_output
+from quantopian.pipeline.data.builtin import USEquityPricing
+from quantopian.pipeline.data import morningstar
+from quantopian.pipeline.factors import SimpleMovingAverage, AverageDollarVolume
+from quantopian.pipeline.filters.morningstar import IsPrimaryShare
 
 import numpy as np  # needed for NaN handling
 import math  # ceil and floor are useful for rounding
 
 from itertools import cycle
 
-import logbook
-
-log = logbook.Logger('algo')
-
-
-def record(*args, **kwargs):
-    print('args={}, kwargs={}'.format(args, kwargs))
-
 
 def initialize(context):
+    # set_commission(commission.PerShare(cost=0.01, min_trade_cost=1.50))
+    set_slippage(
+        slippage.VolumeShareSlippage(
+            volume_limit=.20,
+            price_impact=0.0))
+    # set_slippage(slippage.FixedSlippage(spread=0.00))
+    set_commission(commission.PerTrade(cost=0.00))
+    # set_slippage(slippage.FixedSlippage(spread=0.00))
+    set_long_only()
 
-    context.MaxCandidates = 40
+    context.MaxCandidates = 100
     context.MaxBuyOrdersAtOnce = 30
     context.MyLeastPrice = 3.00
     context.MyMostPrice = 25.00
@@ -44,13 +31,13 @@ def initialize(context):
 
     # over simplistic tracking of position age
     context.age = {}
-    print(len(context.portfolio.positions))
+    print len(context.portfolio.positions)
 
     # Rebalance
     EveryThisManyMinutes = 10
     TradingDayHours = 6.5
     TradingDayMinutes = int(TradingDayHours * 60)
-    for minutez in range(
+    for minutez in xrange(
         1,
         TradingDayMinutes,
         EveryThisManyMinutes
@@ -75,6 +62,9 @@ def initialize(context):
         date_rules.every_day(),
         time_rules.market_close())
 
+    # Create our pipeline and attach it to our algorithm.
+    my_pipe = make_pipeline(context)
+    attach_pipeline(my_pipe, 'my_pipeline')
 
 
 def make_pipeline(context):
@@ -85,16 +75,34 @@ def make_pipeline(context):
     # Filter for primary share equities. IsPrimaryShare is a built-in filter.
     primary_share = IsPrimaryShare()
 
+    # Equities listed as common stock (as opposed to, say, preferred stock).
+    # 'ST00000001' indicates common stock.
+    common_stock = morningstar.share_class_reference.security_type.latest.eq(
+        'ST00000001')
+
+    # Non-depositary receipts. Recall that the ~ operator inverts filters,
+    # turning Trues into Falses and vice versa
+    not_depositary = ~morningstar.share_class_reference.is_depositary_receipt.latest
+
+    # Equities not trading over-the-counter.
+    not_otc = ~morningstar.share_class_reference.exchange_id.latest.startswith(
+        'OTC')
+
     # Not when-issued equities.
-    not_wi = ~IEXCompany.symbol.latest.endswith('.WI')
+    not_wi = ~morningstar.share_class_reference.symbol.latest.endswith('.WI')
 
     # Equities without LP in their name, .matches does a match using a regular
     # expression
-    not_lp_name = ~IEXCompany.companyName.latest.matches('.* L[. ]?P.?$')
+    not_lp_name = ~morningstar.company_reference.standard_name.latest.matches(
+        '.* L[. ]?P.?$')
+
+    # Equities with a null value in the limited_partnership Morningstar
+    # fundamental field.
+    not_lp_balance_sheet = morningstar.balance_sheet.limited_partnership.latest.isnull()
 
     # Equities whose most recent Morningstar market cap is not null have
     # fundamental data and therefore are not ETFs.
-    have_market_cap = IEXKeyStats.marketcap.latest >= 1
+    have_market_cap = morningstar.valuation.market_cap.latest.notnull()
 
     # At least a certain price
     price = USEquityPricing.close.latest
@@ -104,8 +112,12 @@ def make_pipeline(context):
     # Filter for stocks that pass all of our previous filters.
     tradeable_stocks = (
         primary_share
+        & common_stock
+        & not_depositary
+        & not_otc
         & not_wi
         & not_lp_name
+        & not_lp_balance_sheet
         & have_market_cap
         & AtLeastPrice
         & AtMostPrice
@@ -178,7 +190,7 @@ def before_trading_start(context, data):
     context.MyCandidate = cycle(context.stocks_worst)
 
     context.LowestPrice = context.MyLeastPrice  # reset beginning of day
-    print(len(context.portfolio.positions))
+    print len(context.portfolio.positions)
     for stock in context.portfolio.positions:
         CurrPrice = float(data.current([stock], 'price'))
         if CurrPrice < context.LowestPrice:
@@ -248,6 +260,24 @@ def my_rebalance(context, data):
                           style=LimitOrder(SellPrice)
                           )
 
+    WeightThisBuyOrder = float(1.00 / context.MaxBuyOrdersAtOnce)
+    for ThisBuyOrder in range(context.MaxBuyOrdersAtOnce):
+        stock = context.MyCandidate.next()
+        PH = data.history([stock], 'price', 20, '1d')
+        PH_Avg = float(PH.mean())
+        CurrPrice = float(data.current([stock], 'price'))
+        if np.isnan(CurrPrice):
+            pass  # probably best to wait until nan goes away
+        else:
+            if CurrPrice > float(1.25 * PH_Avg):
+                BuyPrice = float(CurrPrice)
+            else:
+                BuyPrice = float(CurrPrice * BuyFactor)
+            BuyPrice = float(make_div_by_05(BuyPrice, buy=True))
+            StockShares = int(WeightThisBuyOrder * cash / BuyPrice)
+            order(stock, StockShares,
+                  style=LimitOrder(BuyPrice)
+                  )
 
 # if cents not divisible by .05, round down if buy, round up if sell
 
@@ -269,8 +299,8 @@ def my_record_vars(context, data):
     record(positions=len(context.portfolio.positions))
     if 0 < len(context.age):
         MaxAge = context.age[max(
-            list(context.age.keys()), key=(lambda k: context.age[k]))]
-        print(MaxAge)
+            context.age.keys(), key=(lambda k: context.age[k]))]
+        print MaxAge
         record(MaxAge=MaxAge)
     record(LowestPrice=context.LowestPrice)
 
@@ -279,7 +309,7 @@ def log_open_order(StockToLog):
     oo = get_open_orders()
     if len(oo) == 0:
         return
-    for stock, orders in oo.items():
+    for stock, orders in oo.iteritems():
         if stock == StockToLog:
             for o in orders:
                 message = 'Found open order for {amount} shares in {stock}'
@@ -290,7 +320,7 @@ def log_open_orders():
     oo = get_open_orders()
     if len(oo) == 0:
         return
-    for stock, orders in oo.items():
+    for stock, orders in oo.iteritems():
         for o in orders:
             message = 'Found open order for {amount} shares in {stock}'
             log.info(message.format(amount=o.amount, stock=stock))
@@ -300,7 +330,7 @@ def cancel_open_buy_orders(context, data):
     oo = get_open_orders()
     if len(oo) == 0:
         return
-    for stock, orders in oo.items():
+    for stock, orders in oo.iteritems():
         for o in orders:
             # message = 'Canceling order of {amount} shares in {stock}'
             # log.info(message.format(amount=o.amount, stock=stock))
@@ -312,7 +342,7 @@ def cancel_open_orders(context, data):
     oo = get_open_orders()
     if len(oo) == 0:
         return
-    for stock, orders in oo.items():
+    for stock, orders in oo.iteritems():
         for o in orders:
             # message = 'Canceling order of {amount} shares in {stock}'
             # log.info(message.format(amount=o.amount, stock=stock))
